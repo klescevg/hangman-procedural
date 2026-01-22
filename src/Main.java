03807e9ddd3fead8822df0b5224257a23faf089c
klescevg <evgeniy.kleschenko@gmail.com>
2026-01-22 20:28:45 +0100

    Create project structure

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void startGameRound() {
+        // read dictionary
+
+        // game loop
+        // select random word
+        // play round
+    }
+
+    public void playRound() {
+        // round loop
+        // guess letter
+        // check letter presence
+        // show result after each guess
+    }
+
+    public static void readDictionary() {
+        // scanner only words with length>4
+
+        // store them to array
+    }
+
+    public static void getRandomWord() {
+        // returns random word from the dictionary
+    }
+
+    public static void inputLetter() {
+        // user types the letter he wants to check
+    }
+
+    public static void containsLetter() {
+        // checks if the word contains provided letter
+    }
+
+    public static void showResults() {
+        // show mistakes count
+        // show current hangman state
+        // show
+    }
+
+
+}

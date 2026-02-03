efd11ade4973414bd49ffaaf015f3e40c5bc3526
klescevg <evgeniy.kleschenko@gmail.com>
2026-02-03 20:15:38 +0100

    Implement method to get random word from dictionary

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,14 @@ import java.util.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Main {
+    private static int MIN_WORD_SIZE = 4;
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Random random = new Random();
 
     public static void main(String[] args) throws IOException {
         List<String> dictionary = readDictionary();
+        System.out.println("Random word: " + getRandomWord(dictionary));
         System.out.println("Hello world!");
     }
 
@@ -34,7 +39,7 @@ public class Main {
         Scanner scanner = new Scanner(Path.of("russian_nouns.txt"), UTF_8);
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
-            if (input.length() > 4) {
+            if (input.length() >= MIN_WORD_SIZE) {
                 dictionary.add(input);
             }
         }
@@ -42,8 +47,10 @@ public class Main {
         return dictionary;
     }
 
-    public static void getRandomWord() {
-        // returns random word from the dictionary
+    // returns random word from the dictionary
+    public static String getRandomWord(List<String> dictionary) {
+        int randomInt = random.nextInt(dictionary.size());
+        return dictionary.get(randomInt);
     }
 
     public static void inputLetter() {
